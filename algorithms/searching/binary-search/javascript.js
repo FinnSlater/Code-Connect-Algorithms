9fca6309eee5f2372926eed7593082d44a01c750
Finn Slater <90759033+FinnSlater@users.noreply.github.com>
2025-03-04 21:44:33 +0000

    Add initial folders and binary search in JS

---
+++ algorithms/searching/binary-search/javascript.js
@@ -0,0 +1,23 @@
+// note that list must be sorted for this function to work 
+function binarySearch(sortedArray, key){
+    let start = 0;
+    let end = sortedArray.length - 1;
+
+    while (start <= end) {
+        let middle = Math.floor((start + end) / 2);
+
+        if (sortedArray[middle] === key) {
+            // found the key
+            return middle;
+        } else if (sortedArray[middle] < key) {
+            // continue searching to the right
+            start = middle + 1;
+        } else {
+            // search searching to the left
+            end = middle - 1;
+        }
+    }
+    // key wasn't found
+    return -1;
+}
+console.log("index is: ", binarySearch([-14, 4, 6, 8, 10, 20, 308], 6));

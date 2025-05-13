552a0a0fe71145235d19a21a36ead8557f3494ee
Finn Slater <90759033+FinnSlater@users.noreply.github.com>
2025-05-13 21:16:42 +0100

    Create linear-search javascript.js

---
+++ algorithms/searching/linear-search/javascript.js
@@ -0,0 +1,8 @@
+function linear_search(array=data["arr"], target=data["target"]){
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === target) {
+      return i;
+    }
+  }
+  return -1;
+}
